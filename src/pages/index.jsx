97dbb606f1579002ae6a8c8ccc379617561fb522
holidayman <lehafirstchanel@gmail.com>
2023-06-10 15:10:57 +0300

    layout fixes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
+import { request } from "near-social-bridge";
+const getIsLoggedIn = (payload) => request("is-logged", payload);
 
+getIsLoggedIn()
+  .then((res) => console.log("response is", res))
+  .catch((err) => console.log("error getting is logged in iframe", err));
 export default function Root() {
   return (
     <div className="relative flex h-1/2 min-h-screen flex-col bg-secondary-black p-5 pl-10 pr-10">
@@ -17,7 +22,7 @@ export default function Root() {
           Wallet
         </Link>
       </nav>
-      <main className="flex  flex-col items-center	">
+      <main className="flex  flex-col items-center	max-lg:mt-14">
         <div className="flex items-center justify-center p-12 max-lg:flex-col max-lg:p-3 max-md:p-6">
           <div className=" flex items-center justify-center">
             <Link
@@ -32,7 +37,7 @@ export default function Root() {
           </div>
           <div className="ml-4 flex flex-col items-center justify-between max-lg:ml-0 max-lg:mt-5">
             <Link
-              className=" h-2/3 rounded-lg border border-primary-red bg-secondary-black p-9 text-4xl text-secondary-white max-sm:p-5 max-sm:text-2xl"
+              className=" h-2/3 rounded-lg border-4 border-primary-red bg-secondary-black p-9 text-4xl text-secondary-white max-sm:p-5 max-sm:text-2xl"
               to="/track-package/analytics"
             >
               <img className="m-auto max-sm:w-52" src="/graph.png" alt="" />
@@ -41,7 +46,7 @@ export default function Root() {
               </p>
             </Link>
             <Link
-              className=" mt-3 h-2/3  rounded-lg border border-secondary-white  bg-secondary-black p-12 text-4xl text-secondary-white max-sm:w-64 max-sm:p-5 max-sm:text-2xl"
+              className="mt-3 h-2/3 rounded-lg border-4 border-secondary-white  bg-secondary-black p-12 text-4xl text-secondary-white max-sm:w-64 max-sm:p-5 max-sm:text-2xl"
               to="/scan-package/qr-code-scanning"
             >
               <img className="m-auto" src="/scan.png" alt="" />
