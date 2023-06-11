6399c8aa4b0a9e5dc71bae719782625e87771028
holidayman <lehafirstchanel@gmail.com>
2023-06-11 03:06:42 +0300

    check if admin on change-role

--- src/pages/change-role.jsx
+++ src/pages/change-role.jsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-export default function QrCodeScanning() {
-  const [data, setData] = useState("");
+import { getIsAdmin } from "../contract-requests/getters";
+
+const textForAdmin = `Your current role is "admin", in the purpose of testing during the
+hackathon, you can make yourself back a user, and you will be able
+only to receive packages`;
+
+const textForUser = `Your current role is "user", in the purpose of testing during the
+hackathon, you can make yourself an administrator and be allowed to
+create and manage packages`;
+
+export default function ChangeRole() {
+  const [isAdmin, setIsAdmin] = useState(null);
+
+  useEffect(() => {
+    getIsAdmin()
+      .then((res) => {
+        console.log("is admin", res);
+        setIsAdmin(res);
+      })
+      .catch((err) => console.log("error getting is admin in iframe", err));
+  }, []);
+
   return (
     <div className="relative flex h-1/2 min-h-screen flex-col bg-secondary-black p-5 pl-10 pr-10">
       <nav className="flex items-center text-4xl font-normal leading-[60px] text-secondary-white">
@@ -10,16 +30,18 @@ export default function QrCodeScanning() {
           Back
         </Link>
       </nav>
-      <main className="mt-8 flex flex-col">
-        <h1 className="text-6xl font-normal leading-[96px] text-secondary-white max-md:text-4xl max-sm:text-center">
-          Change role
-        </h1>
-        <p className="mt-8 text-xl font-normal leading-[72px] text-secondary-white">
-          Your current role is "admin", in the purpose of testing during the
-          hackathon, you can make yourself back a user, and you will be able
-          only to receive packages
-        </p>
-        <button className="text-secondary-white">Make me user</button>
+      <main className="flex flex-col items-center">
+        <h2 className="mt-2 w-3/4 text-center text-4xl font-normal leading-[72px] text-secondary-white max-lg:text-4xl max-md:text-3xl max-sm:text-lg">
+          {isAdmin !== null && (isAdmin ? textForAdmin : textForUser)}
+          {isAdmin === null && "Loading..."}
+        </h2>
+        <Link
+          className="m-auto mt-10 h-12 w-44 rounded-lg bg-primary-red text-center text-2xl font-normal leading-[46px] text-secondary-white"
+          to="/consumer-main-page/consumer"
+        >
+          {isAdmin !== null && `Make me ${isAdmin ? "user" : "admin"}`}
+          {isAdmin === null && "Loading..."}
+        </Link>
       </main>
     </div>
   );
